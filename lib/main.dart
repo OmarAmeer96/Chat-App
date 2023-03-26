c4c08787c790080720f3c638561fd247a3b69bd5
Omar_Ameer <omar.ameer244@gmail.com>
2023-03-26 13:59:47 +0200

    14th Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:chat_app/screens/login_screen.dart';
 import 'package:chat_app/screens/register_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'firebase_options.dart';
 
 void main() async {
