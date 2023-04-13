28b272c62e1a81f1786947f18b9f1e5ccecd2c8d
Omar_Ameer <omar.ameer244@gmail.com>
2023-04-13 17:18:02 +0200

    16th Commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../widgets/chat_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   ChatScreen({super.key});
@@ -16,64 +17,74 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: kPrimaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              kLogo,
-              height: 50,
-              width: 50,
+    return FutureBuilder<QuerySnapshot>(
+      future: messages.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          print(snapshot.data!.docs[0]['message']);
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: kPrimaryColor,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    kLogo,
+                    height: 50,
+                    width: 50,
+                  ),
+                  const Text('CHAT'),
+                ],
+              ),
+              centerTitle: true,
             ),
-            const Text('CHAT'),
-          ],
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return const ChatBubble();
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              controller: controller,
-              onSubmitted: (data) {
-                messages.add(
-                  {
-                    'message': data,
-                  },
-                );
-                controller.clear();
-              },
-              decoration: InputDecoration(
-                hintText: 'Send Message',
-                suffixIcon: const Icon(
-                  Icons.send_rounded,
-                  color: kPrimaryColor,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: const BorderSide(
-                    color: kPrimaryColor,
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return const ChatBubble();
+                    },
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      messages.add(
+                        {
+                          'message': data,
+                        },
+                      );
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Send Message',
+                      suffixIcon: const Icon(
+                        Icons.send_rounded,
+                        color: kPrimaryColor,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: const BorderSide(
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
+          );
+        } else {
+          return const Text('Loading....');
+        }
+      },
     );
   }
 }
